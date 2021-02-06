58e97f8ba25e10d4e9ef1eb604cf7fb12938c9eb
benalex99 <benalex@planet.nl>
2021-02-07 00:41:38 +0100

    Added NN description and stubs

---
+++ NeuroEvo/NeuralNetwork.py
@@ -0,0 +1,15 @@
+import Genome
+import torch
+
+# Neural network class for running predictions on the GPU
+class NeuralNetwork:
+
+    layers = []
+
+    # Initialize a network from a Genome
+    def __init__(self, Genome):
+        return
+
+    # Calculates the output of the given input
+    def predict(self, input):
+        return
