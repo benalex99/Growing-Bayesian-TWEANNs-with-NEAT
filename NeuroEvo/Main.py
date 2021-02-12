d2e17d90dc59031150fcfecad7c53c0844fdd195
Fynn Voß <programming@vossboss.org>
2021-02-12 21:26:35 +0100

    Revert "Merge branch 'v0.1/1/neat' into main"
    This reverts commit 9f4ae69eb5742c71a2e86f2cb919bee24bd828a2, reversing
    changes made to 091830fd0e3d370ed125eeb122abc98ffd6a2baa.

--- NeuroEvo/Main.py
+++ NeuroEvo/Main.py
@@ -7,8 +7,20 @@ import torch
 
 # Choose an environment and optimizer, run trainer
 # example nn with layers 5,2,3
+from Optimizers.NEAT.NEATGenome import NEATGenome
+
 layers = [[torch.ones(5,2),torch.ones(2)], [torch.ones(2,3),torch.ones(3)]]
 
 nn = NeuralNetwork(layers)
 
+gg = NEATGenome()
+gg.addNode()
+gg.addNode()
+gg.addNode()
+gg.addNode()
+gg.addEdge()
+
+print(gg.edges)
+
 print(nn.forward([1,2,3,4,5]))
+
---
+++ NeuroEvo/Optimizers/NEAT/NEATGenome.py
@@ -0,0 +1,39 @@
+
+# Decorator for the Genome class. Wraps around the Genome to give it a score, so it can be used in NEAT.
+# Needs to implement a Genome base class.
+import numpy as np
+
+from Genome import Genome, NodeGene, ConnectionGene
+import random
+
+
+class NEATGenome(Genome.Genome):
+
+    def __init__(self):
+        super(NEATGenome, self).__init__()
+
+    # Returns a random Index Number to get.
+    def randomIndex(self, nodeArray) -> float:
+        return nodeArray[random.randint(0, len(nodeArray)-1)]
+
+    # Mutate by adding an edge or node, or tweak a weight
+    def mutate(self):
+        return
+
+    # Add an edge to connect two nodes
+    def addEdge(self):
+        firstNodeToConnect = self.randomIndex(self.nodes)
+        secondNodeToConnect = self.randomIndex(self.nodes)
+        while secondNodeToConnect == firstNodeToConnect:
+            secondNodeToConnect = self.randomIndex(self.nodes)
+        self.edges.append(ConnectionGene.EdgeGene(firstNodeToConnect, secondNodeToConnect, ((random.random()*2)-1)))
+
+    # Replace an edge by a node with the incoming edge having weight 1
+    # and the outgoing edge having the original edges weight
+    def addNode(self):
+        self.nodes.append(NodeGene.NodeGene(len(self.nodes)))
+        return
+
+    # Tweak a random weight by adding Gaussian noise
+    def tweakWeight(self):
+        return
