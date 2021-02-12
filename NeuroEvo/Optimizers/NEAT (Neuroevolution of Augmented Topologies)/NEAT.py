c12adb922157959b5e894cef3a06f819d7fdf6e2
benalex99 <benalex@planet.nl>
2021-02-12 17:22:55 +0100

    fixed small mistake

---
+++ NeuroEvo/Optimizers/NEAT (Neuroevolution of Augmented Topologies)/NEAT.py
@@ -0,0 +1,20 @@
+
+
+class NEAT:
+
+# Mutate by adding an edge or node, or tweak a weight
+    def mutate(genome):
+        return
+
+    # Add an edge connection two nodes
+    def addEdge(genome):
+        return
+
+    # Replace an edge by a node with the incoming edge having weight 1
+    # and the outgoing edge having the original edges weight
+    def addNode(genome):
+        return
+
+    # Tweak a random weight by adding Gaussian noise
+    def tweakWeight(genome):
+        return
