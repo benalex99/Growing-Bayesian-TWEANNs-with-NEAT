0590dc0f4be2e2fde494c6a9ed21dd4f5ce45115
Fynn Voß <programming@vossboss.org>
2021-02-12 21:12:13 +0100

    Revert "Merge branch 'v0.1/1/neat'"
    This reverts commit 5def32666c6ab3bd5b78d6cd743170548f5f5b7b.
    
    Conflicts:
    	.idea/STA.iml

--- NeuroEvo/Main.py
+++ NeuroEvo/Main.py
@@ -7,20 +7,8 @@ import torch
 
 # Choose an environment and optimizer, run trainer
 # example nn with layers 5,2,3
-from NeuroEvo.Optimizers.NEAT.NEATGenome import NEATGenome
-
 layers = [[torch.ones(5,2),torch.ones(2)], [torch.ones(2,3),torch.ones(3)]]
 
 nn = NeuralNetwork(layers)
 
-gg = NEATGenome()
-gg.addNode()
-gg.addNode()
-gg.addNode()
-gg.addNode()
-gg.addEdge()
-
-print(gg.edges)
-
 print(nn.forward([1,2,3,4,5]))
-
