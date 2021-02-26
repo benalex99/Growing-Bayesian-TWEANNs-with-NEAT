02f6cc67df13dbfcab927b85b39ac4016d9179fd
Benjamin <benjaminforge716@gmail.com>
2021-02-27 00:17:49 +0100

    basic shtt

--- NeuroEvo/Optimizers/NEAT/NEAT.py
+++ NeuroEvo/Optimizers/NEAT/NEAT.py
@@ -3,11 +3,10 @@ from NeuroEvo.Optimizers.NEAT.NEATGenome import NEATGenome
 import numpy as np
 import time
 
-
 class NEAT:
     population: list
 
-    def __init__(self, iterations, batchSize, maxPopSize, episodeDur, showProgress=(0, 0)):
+    def __init__(self, iterations, batchSize, maxPopSize, episodeDur, showProgress = (0,0)):
         self.population = []
         self.iterations = iterations
         self.batchSize = batchSize
@@ -16,10 +15,10 @@ class NEAT:
         self.showProgress = showProgress
         return
 
-    def run(self, rootGenome, env, seed=0):
+    def run(self,rootGenome, env, seed= 0):
         self.hMarker = 1
         self.population.append(rootGenome)
-        self.visualize(rootGenome, env, 500, useDone=False)
+        self.visualize(rootGenome, env, 500, useDone= False)
         for iter in range(self.iterations):
             print("Iteration: " + str(iter))
             ntime = time.time()
@@ -32,23 +31,23 @@ class NEAT:
                 avgSize += len(g.edges)
                 toBeTested.append(g)
 
-            print("AvgSize : " + str(avgSize / len(toBeTested)))
+            print("AvgSize : " + str(avgSize/len(toBeTested)))
 
             # Assign values to the mutations and add them to the population
-            env.test(toBeTested, self.episodeDur, seed=iter)
+            env.test(toBeTested, self.episodeDur, seed= iter)
             self.population = self.population + toBeTested
             self.population.sort(key=lambda x: x.fitness, reverse=True)
 
             # Discard bad mutations from the population until the max population count is reached
             while (len(self.population) > self.maxPopSize):
-                i = random.randint(max(0, int(len(self.population) / 2 - 1)), len(self.population) - 1)
+                i = random.randint(max(0, int(len(self.population)/2 - 1)), len(self.population) - 1)
                 self.population.remove(self.population[i])
             self.median = self.population[int(len(self.population) / 2)].fitness
             print("Took : " + str(time.time() - ntime))
             print("Median score: " + str(self.median))
             print("Best Score: " + str(self.bestGene().fitness) + "\n")
-            if (self.showProgress[0] > 0 and iter % self.showProgress[0] == 0):
-                self.visualize(self.bestGene(), env, self.showProgress[1], seed=iter)
+            if(self.showProgress[0] > 0 and iter % self.showProgress[0] == 0):
+                self.visualize(self.bestGene(),env, self.showProgress[1], seed= iter)
 
         # Get the best gene from the population
         return self.bestGene()
@@ -59,7 +58,7 @@ class NEAT:
         weakestGenome: NEATGenome = firstGenome.copy() if firstGenome.fitness < secondGenome.fitness else secondGenome.copy()
 
         for index, value in enumerate(weakestGenome.edges):
-            if fittestGenome.edges[min(index, len(fittestGenome.edges) - 1)].hMarker == value.hMarker:
+            if fittestGenome.edges[min(index, len(fittestGenome.edges)-1)].hMarker == value.hMarker:
                 if random.randint(0, 1) < 1:
                     fittestGenome.edges[index] = value
             elif firstGenome.fitness == secondGenome.fitness:
@@ -70,8 +69,8 @@ class NEAT:
                     if (fittestGenome.nodes[value.fromNr].layer >= fittestGenome.nodes[value.toNr].layer):
                         fittestGenome.edges.append(value)
                         fittestGenome.nodes[value.fromNr].outputtingTo.append(value.toNr)
-                        fittestGenome.increaseLayers(fittestGenome.nodes[value.fromNr],
-                                                     fittestGenome.nodes[value.toNr])
+                        fittestGenome.increaseLayers(fittestGenome.nodes[value.fromNr], fittestGenome.nodes[value.toNr])
+
 
         return fittestGenome
 
@@ -83,9 +82,9 @@ class NEAT:
                 bestGene = genome
         return bestGene
 
-    def visualize(self, gene, env, duration, useDone=True, seed=0):
+    def visualize(self, gene, env, duration, useDone = True, seed = 0):
         gene.visualize()
-        env.visualize(gene, duration=duration, useDone=useDone, seed=seed)
+        env.visualize(gene, duration= duration, useDone = useDone, seed = seed)
 
     def newGenome(self):
         if (random.randint(0, 1) < 1 or len(self.population) <= 1):
