934b2469a5094b1e03f39042ace7e062d191a630
Fynn Voß <programming@vossboss.org>
2021-02-27 00:12:08 +0100

    Push

--- NeuroEvo/Main.py
+++ NeuroEvo/Main.py
@@ -9,11 +9,11 @@ import torch
 optim = NEAT(iterations= 1000000000000, batchSize= 200, maxPopSize= 400, episodeDur= 1000, showProgress= (1,1000))
 # env = GymEnv('CartPole-v0')
 # env = GymEnv('MountainCar-v0')
-#env = GymEnv('LunarLander-v2')
+env = GymEnv('LunarLander-v2')
 # env = GymEnv('LunarLanderContinuous-v2')
 # env = GymEnv('Acrobot-v1')
 # env = GymEnv('Pendulum-v0')
-env = GymEnv('MountainCarContinuous-v0')
+# env = GymEnv('MountainCarContinuous-v0')
 
 print(env.outputs())
 gg, score = Trainer.run(optim,env)
--- NeuroEvo/Optimizers/NEAT/NEAT.py
+++ NeuroEvo/Optimizers/NEAT/NEAT.py
@@ -3,10 +3,11 @@ from NeuroEvo.Optimizers.NEAT.NEATGenome import NEATGenome
 import numpy as np
 import time
 
+
 class NEAT:
     population: list
 
-    def __init__(self, iterations, batchSize, maxPopSize, episodeDur, showProgress = (0,0)):
+    def __init__(self, iterations, batchSize, maxPopSize, episodeDur, showProgress=(0, 0)):
         self.population = []
         self.iterations = iterations
         self.batchSize = batchSize
@@ -15,10 +16,10 @@ class NEAT:
         self.showProgress = showProgress
         return
 
-    def run(self,rootGenome, env, seed= 0):
+    def run(self, rootGenome, env, seed=0):
         self.hMarker = 1
         self.population.append(rootGenome)
-        self.visualize(rootGenome, env, 500, useDone= False)
+        self.visualize(rootGenome, env, 500, useDone=False)
         for iter in range(self.iterations):
             print("Iteration: " + str(iter))
             ntime = time.time()
@@ -31,23 +32,23 @@ class NEAT:
                 avgSize += len(g.edges)
                 toBeTested.append(g)
 
-            print("AvgSize : " + str(avgSize/len(toBeTested)))
+            print("AvgSize : " + str(avgSize / len(toBeTested)))
 
             # Assign values to the mutations and add them to the population
-            env.test(toBeTested, self.episodeDur, seed= iter)
+            env.test(toBeTested, self.episodeDur, seed=iter)
             self.population = self.population + toBeTested
             self.population.sort(key=lambda x: x.fitness, reverse=True)
 
             # Discard bad mutations from the population until the max population count is reached
             while (len(self.population) > self.maxPopSize):
-                i = random.randint(max(0, int(len(self.population)/2 - 1)), len(self.population) - 1)
+                i = random.randint(max(0, int(len(self.population) / 2 - 1)), len(self.population) - 1)
                 self.population.remove(self.population[i])
             self.median = self.population[int(len(self.population) / 2)].fitness
             print("Took : " + str(time.time() - ntime))
             print("Median score: " + str(self.median))
             print("Best Score: " + str(self.bestGene().fitness) + "\n")
-            if(self.showProgress[0] > 0 and iter % self.showProgress[0] == 0):
-                self.visualize(self.bestGene(),env, self.showProgress[1], seed= iter)
+            if (self.showProgress[0] > 0 and iter % self.showProgress[0] == 0):
+                self.visualize(self.bestGene(), env, self.showProgress[1], seed=iter)
 
         # Get the best gene from the population
         return self.bestGene()
@@ -58,20 +59,19 @@ class NEAT:
         weakestGenome: NEATGenome = firstGenome.copy() if firstGenome.fitness < secondGenome.fitness else secondGenome.copy()
 
         for index, value in enumerate(weakestGenome.edges):
-            if fittestGenome.edges[min(index, len(fittestGenome.edges)-1)].hMarker == value.hMarker:
+            if fittestGenome.edges[min(index, len(fittestGenome.edges) - 1)].hMarker == value.hMarker:
                 if random.randint(0, 1) < 1:
                     fittestGenome.edges[index] = value
             elif firstGenome.fitness == secondGenome.fitness:
-
-                if (not (fittestGenome.nodes[min(value.toNr, len(fittestGenome.nodes) - 1)].nodeNr == value.toNr)):
-                    if (fittestGenome.nodes[value.fromNr].layer >= weakestGenome.nodes[value.toNr].layer):
-                        fittestGenome.nodes.insert(value.toNr, weakestGenome.nodes[value.toNr].__deepcopy__())
-                if(len(fittestGenome.nodes) < value.toNr):
-                    if(fittestGenome.nodes[value.fromNr].layer >= fittestGenome.nodes[value.toNr].layer):
+                if (len(fittestGenome.nodes) > value.toNr and len(fittestGenome.nodes) > value.fromNr):
+                    if (not (fittestGenome.nodes[min(value.toNr, len(fittestGenome.nodes) - 1)].nodeNr == value.toNr)):
+                        if (fittestGenome.nodes[value.fromNr].layer >= weakestGenome.nodes[value.toNr].layer):
+                            fittestGenome.nodes.insert(value.toNr, weakestGenome.nodes[value.toNr].__deepcopy__())
+                    if (fittestGenome.nodes[value.fromNr].layer >= fittestGenome.nodes[value.toNr].layer):
                         fittestGenome.edges.append(value)
                         fittestGenome.nodes[value.fromNr].outputtingTo.append(value.toNr)
-                        fittestGenome.increaseLayers(fittestGenome.nodes[value.fromNr], fittestGenome.nodes[value.toNr])
-
+                        fittestGenome.increaseLayers(fittestGenome.nodes[value.fromNr],
+                                                     fittestGenome.nodes[value.toNr])
 
         return fittestGenome
 
@@ -83,9 +83,9 @@ class NEAT:
                 bestGene = genome
         return bestGene
 
-    def visualize(self, gene, env, duration, useDone = True, seed = 0):
+    def visualize(self, gene, env, duration, useDone=True, seed=0):
         gene.visualize()
-        env.visualize(gene, duration= duration, useDone = useDone, seed = seed)
+        env.visualize(gene, duration=duration, useDone=useDone, seed=seed)
 
     def newGenome(self):
         if (random.randint(0, 1) < 1 or len(self.population) <= 1):
